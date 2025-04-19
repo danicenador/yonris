f1909307184ddff70b85b44cc9296a96daab167e
Your Name <you@example.com>
2025-04-19 18:10:30 +0200

    forgoten to add block.rs to the commit

---
+++ src/block.rs
@@ -0,0 +1,32 @@
+use crate::ivec2::IVec2;
+use crate::piece::PieceType;
+
+pub struct Block {
+    pub position: IVec2,
+    pub piece_type: PieceType,
+    pub is_core: bool,
+}
+
+impl Block {
+    pub fn new(position: IVec2, piece_type: PieceType, is_core: bool) -> Self {
+        Block {
+            position,
+            piece_type,
+            is_core,
+        }
+    }
+
+    pub fn projection_left(&self) -> IVec2 {
+        IVec2::new(self.position.x - 1, self.position.y)
+    }
+    pub fn projection_right(&self) -> IVec2 {
+        IVec2::new(self.position.x + 1, self.position.y)
+    }
+    pub fn projection_down(&self) -> IVec2 {
+        IVec2::new(self.position.x, self.position.y + 1)
+    }
+
+    pub fn set_position(&mut self, new_possition: IVec2) {
+        self.position = new_possition;
+    }
+}
